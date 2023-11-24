edc822e4ed882f1e2b9f60e0fc2c1f7882edff51
Phanuel Omamowho <117802490+NewPhanuel@users.noreply.github.com>
2023-11-24 15:24:38 +0100

    Update proc_file_comm.c

--- proc_file_comm.c
+++ proc_file_comm.c
@@ -7,6 +7,7 @@ int proc_file_commands(char *file_path, int *exe_ret);
  * cant_open - If the file doesn't exist or lacks proper permissions, print
  * a cant open error.
  * @file_path: Path to the supposed file.
+ * In collaboration with Dev Phanuel
  *
  * Return: 127.
  */
