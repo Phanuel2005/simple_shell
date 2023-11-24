98f8df282a8142690e9a35eb56c49b9e622b03df
Phanuel Omamowho <117802490+NewPhanuel@users.noreply.github.com>
2023-11-24 15:18:50 +0100

    Update builtins.c

--- builtins.c
+++ builtins.c
@@ -8,6 +8,7 @@ int shellby_help(char **args, char __attribute__((__unused__)) **front);
  * get_builtin - Matches a command with a corresponding
  *               shellby builtin function.
  * @command: The command to match.
+ * In collaboration with Dev Phanuel
  *
  * Return: A function pointer to the corresponding builtin.
  */
